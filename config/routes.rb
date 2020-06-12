41ea37a260b7ae28cf076df4dfffd3dc08a0f255
tsukuruyamamoto387 <y0410188@yahoo.co.jp>
2020-06-12 07:46:46 +0000

    05

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'pages#index'
@@ -6,6 +8,13 @@ Rails.application.routes.draw do
   get 'pages/help'
   
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  
+  
 end
 
 
